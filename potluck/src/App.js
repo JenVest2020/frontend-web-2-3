34689a78e77e28213c4cf2dad5cfa89a1e5fceab
JenVest2020 <jenvest2019@gmail.com>
2020-08-19 03:21:42 -0500

    folder structure started

--- potluck/src/App.js
+++ potluck/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Over-Write These 2 text Elements and Start Adding your Components in Routes!</h1>
+      <h2>Above all: HAVE FUN!!!</h2>
     </div>
   );
 }
